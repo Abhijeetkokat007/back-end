6533e18985fc622589e491bc612dc26316148fef
AbhijeetKokat <abhijeetkokat2003@gmail.com>
2023-10-19 21:26:25 +0530

    added backend

--- index.js
+++ index.js
@@ -1,24 +1,119 @@
-// console.log('backent running');
-import  Express  from "express";
+import Express from "express";
 
 const app = Express();
 
 app.use(Express.json());
-app.post('/student', (req, res)=>{
 
-    const {name, number} = req.query
+const PORT = 5000;
+
+const students = [];
+
+app.get('/students', (req, res) => {
+
+
 
     res.json({
-        information : `my name is ${name}. my mo number is ${number}`
+        success: true,
+        data: students,
+        message: `successfully fecth data. `,
+
+    });
+});
+
+app.post('/student', (req, res) => {
+    const { name, age, number, email } = req.body;
+
+    if(!name){
+       
+        return res.json({
+            success: false,
+            message: `name is required `,
+    
+        });
+    }
+
+    if(!number){
+        
+        return res.json({
+            success: false,
+            message: `number is required `,
+    
+        });
+    }
+
+    if(!age){
+     
+        return res.json({
+            success: false,
+            message: `age is required `,
+    
+        });
+    }
+
+    if(!email){
+      
+        return res.json({
+            success: false,
+            message: `email is required `,
+    
+        });
+    }
+
+    const id = Math.floor(Math.random() * 1000) + 1;
+
+    const newstudent = {
+        id,
+        name,
+        number,
+        age,
+        email
+
+    }
+
+    students.push(newstudent);
+
+    res.json({
+        success: true,
+        data: newstudent,
+        message: `successfully added new data. `,
+
+    });
+    
+})
+
+   
+app.get('/boy', (req, res)=>{
+    const {id} = req.query;
+    let boy = null;
+
+students.forEach((stud)=>{
+    if(stud.id == id){
+        boy = stud;  
+    }
+
+})
+
+if(boy == null){
+
+    return res.json({
+        success: false,
+            message: `student not found `,
     })
+}
 
-    console.log(req.query)
-    console.log(req.body)
-
+res.json({
+    success: true,
+    data: boy,
+        message: `successfully added new student. `,
 })
+   
 
-const PORT = 5050;
 
-app.listen(PORT, ()=>{
-    console.log(` server running on port ${PORT} `)
-})
+} )
+
+
+
+
+    app.listen(PORT, () => {
+        console.log(` server running on port ${PORT} `);
+    })
